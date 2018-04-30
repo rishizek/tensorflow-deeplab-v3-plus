163b51e44ba1db0f6787d063219e5598f1424397
Riei Ishizeki <rishizek@gmail.com>
2018-04-30 15:45:22 +0900

    Replace non-ASCII charactor '\xc3' to ASCII charactor 'x'.

--- deeplab_model.py
+++ deeplab_model.py
@@ -43,7 +43,7 @@ def atrous_spatial_pyramid_pooling(inputs, output_stride, batch_norm_decay, is_t
     with tf.contrib.slim.arg_scope(resnet_v2.resnet_arg_scope(batch_norm_decay=batch_norm_decay)):
       with arg_scope([layers.batch_norm], is_training=is_training):
         inputs_size = tf.shape(inputs)[1:3]
-        # (a) one 1×1 convolution and three 3×3 convolutions with rates = (6, 12, 18) when output stride = 16.
+        # (a) one 1x1 convolution and three 3x3 convolutions with rates = (6, 12, 18) when output stride = 16.
         # the rates are doubled when output stride = 8.
         conv_1x1 = layers_lib.conv2d(inputs, depth, [1, 1], stride=1, scope="conv_1x1")
         conv_3x3_1 = layers_lib.conv2d(inputs, depth, [3, 3], stride=1, rate=atrous_rates[0], scope='conv_3x3_1')
@@ -54,7 +54,7 @@ def atrous_spatial_pyramid_pooling(inputs, output_stride, batch_norm_decay, is_t
         with tf.variable_scope("image_level_features"):
           # global average pooling
           image_level_features = tf.reduce_mean(inputs, [1, 2], name='global_average_pooling', keepdims=True)
-          # 1×1 convolution with 256 filters( and batch normalization)
+          # 1x1 convolution with 256 filters( and batch normalization)
           image_level_features = layers_lib.conv2d(image_level_features, depth, [1, 1], stride=1, scope='conv_1x1')
           # bilinearly upsample features
           image_level_features = tf.image.resize_bilinear(image_level_features, inputs_size, name='upsample')
